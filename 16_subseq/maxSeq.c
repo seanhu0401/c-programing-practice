db43288b00c5ff5859322eecb428485da2104c01
Coursera Learner <nobody@nowhere.nul>
2020-11-02 04:05:23 +0000

    update v3

--- 16_subseq/maxSeq.c
+++ 16_subseq/maxSeq.c
@@ -12,18 +12,13 @@ size_t maxSeq(int * array, size_t n){
     if (array[i]<array[i+1]){
       max_length++;
     }
-    /*
     else{
       if (current_max < max_length){
       current_max = max_length;
       }
       max_length = 1;
     }
-    */
-    if(current_max < max_length){
-      current_max = max_length;
-      max_length = 1;
-    }
+    if(current_max < max_length) current_max = max_length;
   }
   return current_max;
 }
